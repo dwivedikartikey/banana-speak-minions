20e1924f821992ca60f95f0b88112f69f37d657a
Kartikey Dwivedi <kartikeydwivedipara@gmail.com>
2020-12-01 20:36:36 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// console.log("Hello! script is running from different file");
+
+// input
+var username = prompt("Give me your username");
+
+// processing
+var welcomeMessage = "this script works" + username;
+
+// output
+alert("This script works " + username);
