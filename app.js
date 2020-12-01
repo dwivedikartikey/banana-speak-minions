e9d3dc274ff050faefe99a3f547a824b3d2ef824
Kartikey Dwivedi <kartikeydwivedipara@gmail.com>
2020-12-01 23:58:32 +0530

    input output processing

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
 
 btnTranslate.addEventListener("click", function clickEventHandler() {
-    console.log("Clicked.");
-    console.log("input", txtInput.value);
+    outputDiv.innerText = "jhjfhd " + txtInput.value;
 })
 
 var txtInput = document.querySelector("#txt-input");
 
+var outputDiv = document.querySelector("#output");
