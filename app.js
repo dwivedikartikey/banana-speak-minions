3b19bf2cc736ddb4b553016dfbc52be547e616bc
Kartikey Dwivedi <kartikeydwivedipara@gmail.com>
2020-12-03 19:00:21 +0530

    wif: till now

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
 
-// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-var serverURL = "https://api.funtranslations.com/translate/minion.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 
 function getTranslationURL(text) {
@@ -17,7 +16,7 @@ function errorHandler(error) {
 }
 
 function clickHandler() {
-    
+
     var inputText = txtInput.value; //taking input
 
 
@@ -27,8 +26,8 @@ function clickHandler() {
         .then(json => {
             var translatedText = json.contents.translated;
             outputDiv.innerText = translatedText; //output
-           })
+        })
         .catch(errorHandler)
 };
 
-btnTranslate.addEventListener("click", clickHandler)
+btnTranslate.addEventListener("click", clickHandler);
